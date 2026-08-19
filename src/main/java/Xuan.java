062b64b17866a853ae9ee1a365665df31beb3d8d
zruixuan <2544067255@qq.com>
2026-08-19 16:51:55 +0800

    Level-0: Rename Duke to Xuan and add greeting

---
+++ src/main/java/Xuan.java
@@ -0,0 +1,19 @@
+public class Xuan {
+    public static void main(String[] args) {
+        //my banner
+        String banner = "__  __  _   _    _    _   _\n"
+                + "\\ \\/ / | | | |  / \\  | \\ | |\n"
+                + " \\  /  | | | | / _ \\ |  \\| |\n"
+                + " /  \\  | |_| |/ ___ \\| |\\  |\n"
+                + "/_/\\_\\  \\___//_/   \\_\\_| \\_|\n";
+
+        System.out.println(banner);
+
+        //greeting message
+        System.out.println("Hello! I'm Xuan.");
+        System.out.println("What can I do for you? \n");
+
+        //exiting message
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
